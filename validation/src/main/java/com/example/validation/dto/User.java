4aacba02d0d9667b4da9d513abbc2c1a3889e0bc
eung2098 <jeg8421@naver.com>
2021-10-22 18:11:27 +0900

    validation java file

--- validation/src/main/java/com/example/validation/dto/User.java
+++ validation/src/main/java/com/example/validation/dto/User.java
@@ -1,6 +1,10 @@
 package com.example.validation.dto;
 
+import com.example.validation.annotation.YearMonth;
+
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
 
@@ -15,6 +19,18 @@ public class User {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 다릅니다.")
     private String phoneNumber;
+
+    @YearMonth // default = yyyyMM
+    private String reqYearMonth;
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
 
     public String getName() {
         return name;
@@ -55,6 +71,7 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
 }
